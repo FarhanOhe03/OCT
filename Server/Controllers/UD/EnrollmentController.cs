595e960fe8c10e507707c6764a66ba1956058ba5
Farhan Ohe <farhanto@udel.edu>
2023-11-14 19:09:53 -0500

    added DTO’s working on controllers

---
+++ Server/Controllers/UD/EnrollmentController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class EnrollmentController : BaseController, GenericRestController<EnrollmentDTO>
+    {
+        public EnrollmentController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Delete/{CourseNo}/ {}/ {}/ {}")]
+
+        public async Task<IActionResult> Delete(int KeyVal)
+        {
+
+            Debugger.Launch();
+
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.Courses.Where(x => x.CourseNo == CourseNo).FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.Enrollments.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        public Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Get(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Post([FromBody] EnrollmentDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Put([FromBody] EnrollmentDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
